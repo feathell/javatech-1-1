382aad06008434d5bf12b378a72b4026764ec761
feathell <alexandr.mironets@gmail.com>
2026-02-16 19:52:39 +0500

    date (6.2)

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,16 +1,22 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date dateCreation;
 
     public Post(String text) {
         this.text = text;
+        this.dateCreation = new Date();
     }
 
     public String getText() { return this.text; }
 
     public Integer getLikes() { return this.likes; }
+
+    public Date getDateCreation() { return this.dateCreation; }
 
     public void setText(String text) { this.text = text; }
 
