5f656c14278db3adb914b748f99c5dd0084266c5
feathell <alexandr.mironets@gmail.com>
2026-02-16 19:56:03 +0500

    date (6.2) fix

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -5,18 +5,18 @@ import java.util.Date;
 public class Post {
     private String text;
     private Integer likes;
-    private Date dateCreation;
+    private Date creationDate;
 
     public Post(String text) {
         this.text = text;
-        this.dateCreation = new Date();
+        this.creationDate = new Date();
     }
 
     public String getText() { return this.text; }
 
     public Integer getLikes() { return this.likes; }
 
-    public Date getDateCreation() { return this.dateCreation; }
+    public Date getCreationDate() { return this.creationDate; }
 
     public void setText(String text) { this.text = text; }
 
